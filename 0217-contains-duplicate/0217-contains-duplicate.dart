c3f983b2cf210060558743385f3e0fc4e99cf437
Ahmad Arbash <ahmad.arbash.97@outlook.com>
2024-09-30 19:24:13 +0300

    Time: 376 ms (76.28%), Space: 178 MB (66.01%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.dart
+++ 0217-contains-duplicate/0217-contains-duplicate.dart
@@ -1,12 +1,5 @@
 class Solution {
  bool containsDuplicate(List<int> nums) {
-    Map<int, dynamic> data = {};
-    for (int i = 0; i < nums.length; i++) {
-      if (data.containsKey(nums[i])) {
-        data[nums[i]] += 1;
-      }
-      data.putIfAbsent(nums[i], () => 1);
-    }
-    return data.values.any((e) => e >= 2);
+    return nums.toSet().length != nums.length;
   }
 }
