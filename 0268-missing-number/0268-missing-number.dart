e5e8a7cdb7ac0fe2a82c79843761f161d1e8c3de
Ahmad Arbash <ahmad.arbash.97@outlook.com>
2024-10-04 11:42:11 +0300

    Time: 352 ms (77.56%), Space: 151.6 MB (56.41%) - LeetHub

--- 0268-missing-number/0268-missing-number.dart
+++ 0268-missing-number/0268-missing-number.dart
@@ -1,14 +1,14 @@
 class Solution {
  int missingNumber(List<int> nums) {
-    nums.sort();
     var x = [for (var i = 0; i <= nums.length; i++) i];
-    for (int i = 0; i < nums.length; i++) {
-      if (nums[i] != x[i]) {
-        return x[i];
-      } else if (i == nums.length - 1) {
-        return nums.last + 1;
-      }
+    var totalN = 0;
+    var totalX = 0;
+    for (final n in nums) {
+      totalN += n;
     }
-    return 0;
+    for (final e in x) {
+      totalX += e;
+    }
+    return totalX - totalN;
   }
 }
