595461cf9e426680509deab5a9aad76bf035491b
Ahmad Arbash <ahmad.arbash.97@outlook.com>
2024-10-16 15:14:21 +0300

    Time: 507 ms (63.94%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,3 +1,5 @@
-select top(1) customer_number from orders 
-group by customer_number
-order by count(order_number) desc
+with a as (
+select customer_number,
+rank() over (partition by customer_number order by order_number) as r
+ from orders )
+ select top(1) a.customer_number from a order by a.r desc
