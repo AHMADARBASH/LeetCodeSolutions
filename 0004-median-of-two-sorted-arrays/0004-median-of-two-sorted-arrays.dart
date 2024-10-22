eb28df5e5c3f5a07eb76c72d21099318968dd521
Ahmad Arbash <ahmad.arbash.97@outlook.com>
2024-10-22 19:59:34 +0300

    Time: 24 ms (98.92%), Space: 154.8 MB (47.85%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.dart
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.dart
@@ -1,16 +1,14 @@
 class Solution {
   double findMedianSortedArrays(List<int> nums1, List<int> nums2) {
-   for(int i = 0;i<nums2.length;i++){
-       nums1.add(nums2[i]);
-        }
-      nums1.sort();
-      if(nums1.length %2 !=0){
-          int x = nums1.length~/2;
-          return(nums1[x]).toDouble();
-    }else{
-        int half = nums1.length ~/ 2;
-        double x = (nums1[half-1] + nums1[half]) /2;
-          return x;
+    nums1.addAll(nums2);
+    nums1.sort();
+    if (nums1.length % 2 == 0) {
+      print(nums1[nums1.length ~/ 2]);
+      print(nums1[(nums1.length ~/ 2) + -1]);
+      var res = (nums1[nums1.length ~/ 2] + nums1[(nums1.length ~/ 2) - 1]) / 2;
+      return res;
+    } else {
+      return (nums1[nums1.length ~/ 2] / 1).toDouble();
     }
   }
 }
